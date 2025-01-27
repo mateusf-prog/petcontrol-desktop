15ceda869f89c7e2414e2de37c02b80afb4eaabc
Mateus Fonseca <mateus102006@hotmail.com>
2025-01-27 20:29:56 -0300

    update IRepository

---
+++ PetControlSystem/PetControlSystem/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetControlSystem.Models;
+
+namespace PetControlSystem.Repositories.Interfaces
+{
+    public interface IRepository
+    {
+        DbSet<Product> Products { get; set; }
+        DbSet<Order> Orders { get; set; }
+        DbSet<Customer> Customers { get; set; }
+        DbSet<Pet> Pets { get; set; }
+        DbSet<Agenda> Agendas { get; set; }
+        DbSet<PetSupport> Services { get; set; }
+        DbSet<User> Users { get; set; }
+
+        int SaveChanges();
+    }
+}
